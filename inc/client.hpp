21fbc3851ba30f77f0b2c725eb86aaac775fb6d8
varnt <victorarnt@gmail.com>
2023-12-26 19:12:42 -0300

    Add project structure and Makefile instructions for client cpp and hpp

--- inc/client.hpp
+++ inc/client.hpp
@@ -1,7 +1,15 @@
-#pragma once
-#include <string>
+#pragma once /*auxilia na implementação de include de arquivos com depencencia deste arquivo*/
+#include <string> 
 
 #include "udp.hpp"
 
 PacketType cmdToEnum(std::string cmd);
 int envia(UDP& udp, PacketType type, std::string payload);
+/*Envia para o servidor um protocolo UDP com possibilidade
+de ser um pacote MSG ou DADO*/
+
+
+
+
+
+ 
--- src/client.cpp
+++ src/client.cpp
@@ -25,7 +25,7 @@ int main() {
   cout << "\033[2J\033[1;1H"; //clear screen
   cout << "Bem-vindo ao Twix\n";
   
-  // Leitura do nome do perfil
+  // Leitura do nome do perfil com limitações de nome de usuário definidas no escopo do trabalh
   string name;
   while(name.empty()){
     cout << "Digite seu perfil: @";
@@ -41,14 +41,16 @@ int main() {
 
   // Tentativa de login
   if(udp.login(name) < 0){
+    /*TODO: TRATAR RESPOSTA SERVIDOR QUANDO TIVER 2 SESSOES REALIZADAS*/
     perror("login failed");
     exit(EXIT_FAILURE);
   }
 
   cout << "\033[2J\033[1;1H"; //clear screen
   cout << "Logado como " << name << '\n';
+  cout << "Cada mensagem deve conter no max 140 caracteres \n";
   cout << "Comandos: SEND msg | FOLLOW profile | UNFOLLOW profile" << endl;
-  
+  /*comandos tb aceitam somente a letra inicial do comanto e nao eh necessario usar capslock*/
   while(true){
     // Leitura da mensagem a ser enviada
     cout << "> ";
@@ -56,6 +58,14 @@ int main() {
     getline(cin, input);
 
     // Separa a entrada em comando e argumento
+    /*
+    EXEMPLO DE ENTRADA VALIDA ESPERADA:
+    SEND ola!       (press enter)
+    unfollow @onze  (press enter)
+    f @doze         (press enter)
+    u dois  (press enter)
+    follow quatro         (press enter)
+    */
     size_t it = input.find(' ');
     if(it != string::npos){
       cmd = input.substr(0, it);
@@ -64,13 +74,12 @@ int main() {
     else cmd = input;
 
     // Converte entrada para Enum
+    /*Como cada cmd pode ser digitado de varias formas pelo o usuario, o cmdToEnum visa padronizar o envio desses que traduz o comando para seu numero correspondente */
     // (necessario para usar no switch)
     PacketType command;
     command = cmdToEnum(cmd);
 
-    // Recebe mensagem do cliente
-    unique_ptr<Packet> packet;
-
+    
     switch(command){
     case SEND:
     case FOLLOW:
@@ -119,19 +128,30 @@ int envia(UDP& udp, PacketType type, string payload){
   }
 
   // Cria novo packet
-  auto packet = make_unique<Packet>();
+  // Pacote que cliente cria para enviar ao servidor acesse udp.hpp e procure por typedef struct packet_t{
+  /*
+  //Número de sequência
+  //Timestamp do dado
+  //Tipo do pacote (ex. DATA | CMD)
+  //Comprimento do payload
+  //Perfil que enviou o pacote
+  //Dados da mensagem
+  */
+  //unique_ptr<Packet> packet; /*unique_ptr eh um smart pointer*/
 
-  // Se for FOLLOW ou UNFOLLOW e nao tiver @, adiciona
+  auto packet = make_unique<Packet>(); /*Forma otimizada de usar o unique_ptr por isso o trecho acima foi comentado*/
+
+  // Se for FOLLOW ou UNFOLLOW e nao tiver @, adiciona tratamento para conforto do usuario
   if(type == FOLLOW || type == UNFOLLOW)
     if(payload.at(0) != '@')
       payload.insert(0, 1, '@');
 
   // Preenche os campos do packet
-  packet->timestamp = time(NULL);
+  packet->timestamp = time(NULL); //TEST: caso de erro na ordem de cronologia das mensagens, verificar se o timestamp esta sendo preenchido corretamente 
   packet->type = type;
-  strncpy(packet->payload, payload.c_str(), MAXLEN);
+  strncpy(packet->payload, payload.c_str(), MAXLEN); //MAXLEN que limita os 140 caract. implementado em udp.hpp
   packet->length = strnlen(packet->payload, MAXLEN);
 
   // Envia o pacote
-  return udp.envia(move(packet));
+  return udp.envia(move(packet)); //move eh uma funcao que move o conteudo de um ponteiro para outro
 }
