5afe2661de6f230f61b42194e928fc96dd60345a
Victor Arnt <victorarnt@gmail.com>
2024-01-03 16:59:35 -0300

    add comments for future improvements

--- inc/instances.hpp
+++ inc/instances.hpp
@@ -2,7 +2,8 @@
 #include <netinet/in.h>
 #include <string>
 #include <map>
-
+//ainda nao foi utilizada no projeto, provavelmente sera utilizada
+//usado para gerenciar as instancias do cliente que no maximo deve ter 2
 class Instances{
 private: 
   std::map<std::string, struct sockaddr_in> instancesMap;
@@ -15,5 +16,6 @@ public:
   void closeInstance(std::string profile, struct sockaddr_in cliaddr);
   
   // Retorna a porta de um perfil
+  //TODO como resolver se o perfil tem mais de uma instancia?
   int getPort(std::string profile);
 };
